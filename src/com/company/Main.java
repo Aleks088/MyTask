27c33942964786501c2a1bb339a1cb4a6f0285da
geekbrains <info@geekbrains.ru>
2020-08-22 11:20:21 +0300

    Task

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main() {
+	// write your code here
+    }
+    int a = 10;
+    byte b = 2;
+    short s = 34;
+    long z = 123;
+    boolean val = true;
+    float f = 45.255f;
+    double pi = 3.14;
+    char symb = Ф;
+
+
+}
+
